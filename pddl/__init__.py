f4912edb803db05fa2f50b1db6e1299ded0f6d50
MarcoFavorito <marco.favorito@gmail.com>
2020-07-30 15:59:07 +0200

    make isort and black compatible

--- pddl/__init__.py
+++ pddl/__init__.py
@@ -2,6 +2,13 @@
 
 """Top-level package for pddl."""
 
-from .__version__ import (__author__, __author_email__, __copyright__,
-                          __description__, __license__, __title__, __url__,
-                          __version__)
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
