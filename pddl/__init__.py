84b32ebb63ab4c5b7aa6eba6321e6250f764a1b3
MarcoFavorito <marco.favorito@gmail.com>
2020-07-30 15:45:54 +0200

    update __init__ with isort

--- pddl/__init__.py
+++ pddl/__init__.py
@@ -2,13 +2,6 @@
 
 """Top-level package for pddl."""
 
-from .__version__ import (
-    __author__,
-    __author_email__,
-    __copyright__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-)
+from .__version__ import (__author__, __author_email__, __copyright__,
+                          __description__, __license__, __title__, __url__,
+                          __version__)
