e04895ebd62911a816bd230d43c5bae070b37c1f
MarcoFavorito <marco.favorito@gmail.com>
2020-08-08 17:50:02 +0200

    replace 'Lydia' with 'pddl' in copyright

--- pddl/core.py
+++ pddl/core.py
@@ -7,13 +7,13 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Lydia is distributed in the hope that it will be useful,
+# pddl is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Lydia.  If not, see <https://www.gnu.org/licenses/>.
+# along with pddl.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 """
