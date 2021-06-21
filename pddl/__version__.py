b7c779086dda841c0570bbc13f6c67b12cce7b09
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2021-06-21 11:32:25 +0200

    update __version__.py

--- pddl/__version__.py
+++ pddl/__version__.py
@@ -25,7 +25,7 @@
 __title__ = "pddl"
 __description__ = "PDDL parser"
 __url__ = "https://github.com/whitemech/pddl.git"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "Marco Favorito, Francesco Fuggitti"
 __author_email__ = "favorito@diag.uniroma1.it, fuggitti@diag.uniroma1.it"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
