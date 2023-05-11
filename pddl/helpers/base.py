cd6086d3c1221075a0947cd24867f27129efcfb0
aefrank <aefrank17@gmail.com>
2023-05-10 19:32:15 -0700

    removed trailing space from _typed_parameters for consistency, and deleted the space from readme example to pass testing

--- pddl/helpers/base.py
+++ pddl/helpers/base.py
@@ -98,7 +98,7 @@ def _typed_parameters(parameters) -> str:
             result += f"?{p.name} - {' '.join(map(str, p.type_tags))} "
         else:
             result += str(p) + " "
-    return result
+    return result.strip()
 
 
 class RegexConstrainedString(str):
