793676fbb8c34c22d86bce205991b5a2b23260c4
Christian Muise <christian.muise@gmail.com>
2023-05-31 10:36:20 -0400

    Version bump.

--- pddl/__version__.py
+++ pddl/__version__.py
@@ -15,7 +15,7 @@
 __title__ = "pddl"
 __description__ = "PDDL parser"
 __url__ = "https://github.com/AI-Planning/pddl.git"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Marco Favorito, Francesco Fuggitti, Christian Muise"
 __author_email__ = (
     "marco.favorito@gmail.com, fuggitti@diag.uniroma1.it, christian.muise@queensu.ca"
