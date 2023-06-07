0fae51c510088d92118743d61a0053dc8d190d00
MarcoFavorito <marco.favorito@gmail.com>
2023-06-07 10:23:33 +0200

    test: add problem parsing in test_formatter.py

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -16,8 +16,8 @@ from pathlib import Path
 
 import pytest
 
-from pddl.formatter import domain_to_string
-from tests.conftest import DOMAIN_FILES
+from pddl.formatter import domain_to_string, problem_to_string
+from tests.conftest import DOMAIN_FILES, PROBLEM_FILES
 
 
 @pytest.mark.parametrize("pddl_file", DOMAIN_FILES)
@@ -27,3 +27,12 @@ def test_domain_formatter(domain_parser, pddl_file: Path):
     actual_domain_str = domain_to_string(expected_domain_obj)
     actual_domain_obj = domain_parser(actual_domain_str)
     assert actual_domain_obj == expected_domain_obj
+
+
+@pytest.mark.parametrize("pddl_file", PROBLEM_FILES)
+def test_problem_formatter(problem_parser, pddl_file):
+    """Test generated problem formatting."""
+    expected_problem_obj = problem_parser(pddl_file.read_text())
+    actual_problem_str = problem_to_string(expected_problem_obj)
+    actual_problem_obj = problem_parser(actual_problem_str)
+    assert actual_problem_obj == expected_problem_obj
