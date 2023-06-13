a1ec5b3a852d3d10f5009bb04cc18f2ecb31c907
Marco Favorito <marco.favorito@gmail.com>
2023-06-13 10:39:23 +0200

    fix formatting of typed lists
    - types are now printed correctly in the :constants section
    - in the :objects section, the objects are grouped by type (as in constants)
    - minor fixes in how :init and :goal are printed (they are always printed even if empty).
    
    The code changes added here are to be considered temporary, since a refactoring of the formatting module is required. Nevertheless, the printing of the typing information of constants will remain.

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -13,10 +13,14 @@
 
 """This module contains tests verifying that the formatter outputs are syntactically valid and semantically faithful."""
 from pathlib import Path
+from textwrap import dedent
 
 import pytest
 
+from pddl.core import Domain, Problem
 from pddl.formatter import domain_to_string, problem_to_string
+from pddl.logic import constants
+from pddl.requirements import Requirements
 from tests.conftest import DOMAIN_FILES, PROBLEM_FILES
 
 
@@ -36,3 +40,62 @@ def test_problem_formatter(problem_parser, pddl_file):
     actual_problem_str = problem_to_string(expected_problem_obj)
     actual_problem_obj = problem_parser(actual_problem_str)
     assert actual_problem_obj == expected_problem_obj
+
+
+def test_typed_constants_formatting_in_domain() -> None:
+    """Test that types and typed constants are formatted correctly."""
+    t1, t2, t3 = "type_1", "type_2", "type_3"
+
+    a1, b1, c1 = constants("a b c", type_=t1)
+    d2, e2, f2 = constants("d e f", type_=t2)
+    g3, h3, i3 = constants("g h i", type_=t3)
+    j, k, lc = constants("j k l", type_=None)
+
+    # define the domain object.
+    domain = Domain(
+        "my_domain",
+        requirements=[Requirements.TYPING],
+        types={t1: None, t2: t1, t3: t1},
+        constants=[a1, b1, c1, d2, e2, f2, g3, h3, i3, j, k, lc],
+    )
+
+    domain_str = domain_to_string(domain)
+
+    assert domain_str == dedent(
+        """\
+    (define (domain my_domain)
+        (:requirements :typing)
+        (:types type_2 type_3 - type_1 type_1)
+        (:constants a b c - type_1 d e f - type_2 g h i - type_3 j k l)
+        (:predicates )
+    )"""
+    )
+
+
+def test_typed_objects_formatting_in_problem() -> None:
+    """Test that typed objects are formatted correctly."""
+    t1, t2, t3 = "type_1", "type_2", "type_3"
+
+    a1, b1, c1 = constants("a b c", type_=t1)
+    d2, e2, f2 = constants("d e f", type_=t2)
+    g3, h3, i3 = constants("g h i", type_=t3)
+    j, k, lc = constants("j k l", type_=None)
+
+    problem = Problem(
+        "problem-1",
+        domain_name="my_domain",
+        requirements=[Requirements.TYPING],
+        objects=[a1, b1, c1, d2, e2, f2, g3, h3, i3, j, k, lc],
+    )
+    problem_str = problem_to_string(problem)
+
+    assert problem_str == dedent(
+        """\
+    (define (problem problem-1)
+        (:domain my_domain)
+        (:requirements :typing)
+        (:objects a b c - type_1 d e f - type_2 g h i - type_3 j k l)
+        (:init )
+        (:goal (and))
+    )"""
+    )
