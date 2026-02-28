b21873cd219ec34f1fbf945dca450576e3d3e0cc
Christian Muise <christian.muise@gmail.com>
2026-02-28 00:11:46 -0500

    Initial pass on the new CLI form. (#174)
    * Initial pass on the new CLI form.
    
    * Case insensitive comparison of domain names in domain and problem.
    
    * Check plan w.r.t. domain and problem (#175)
    
    * Check plan w.r.t. domain and problem
    
    * Update README correspondingly
    
    * Removing unused import.
    
    * Another unused import.
    
    ---------
    
    Co-authored-by: Dillon Chen <dillon.chen1@protonmail.com>

--- tests/test_plan.py
+++ tests/test_plan.py
@@ -15,9 +15,13 @@
 import pytest
 
 from pddl import parse_domain
-from pddl.core import Plan
-from pddl.logic.helpers import constants
-from tests.conftest import BLOCKSWORLD_FILES, DOMAIN_FILES
+from pddl.action import Action
+from pddl.core import Domain, Plan, Problem
+from pddl.exceptions import PDDLValidationError
+from pddl.logic.base import Not
+from pddl.logic.helpers import constants, variables
+from pddl.logic.predicates import Predicate
+from tests.conftest import BLOCKSWORLD_FILES
 
 
 class TestPlanEmpty:
@@ -59,16 +63,70 @@ def test_plan_equality():
     assert plan1 != plan3
 
 
-def test_bad_plan_and_domain():
-    """Test instantiation error handling."""
+def test_bad_plan_action_name():
+    """Test validator detects plan actions with invalid names."""
+    a, b, c = constants("a b c")
+    a1_name = "action_1"
+    plan = Plan(actions=[(a1_name, [a, b]), (a1_name, [c, b])])
+
+    x, y = variables("x y")
+    p = Predicate("p", x, y)
+    action_1 = Action("action_a", [x, y], precondition=p, effect=Not(p))
+    domain = Domain("simple_domain", predicates={p}, actions={action_1})
+    problem = Problem("simple_problem", domain, objects={a, b, c})
+
+    with pytest.raises(
+        PDDLValidationError,
+        match="Action schema name of .* not found in domain.",
+    ):
+        plan.check(domain, problem)
+
+
+def test_bad_plan_action_instantiation():
+    """Test validator detects plan actions with invalid number of parameters."""
     a, b, c = constants("a b c")
     a1_name = "action_1"
     a2_name = "action_2"
-    plan = Plan(actions=[(a1_name, [a]), (a2_name, [b, c]), (a1_name, [c])])
-    domain = parse_domain(DOMAIN_FILES[0])
+    plan = Plan(actions=[(a1_name, [a]), (a2_name, [b, c, c]), (a1_name, [c, b, a])])
 
-    with pytest.raises(ValueError):
-        plan.instantiate(domain)
+    x, y, z = variables("x y z")
+    p = Predicate("p", x, y, z)
+    action_1 = Action("action_1", [x, y, z], precondition=p, effect=Not(p))
+    action_2 = Action("action_2", [x, y, z], precondition=Not(p), effect=p)
+    domain = Domain("simple_domain", predicates={p}, actions={action_1, action_2})
+    problem = Problem("simple_problem", domain, objects={a, b, c})
+
+    with pytest.raises(
+        PDDLValidationError,
+        match="Number of parameters of .* does not match with the action schema in the domain.",
+    ):
+        plan.check(domain, problem)
+
+
+def test_bad_plan_objects():
+    """Test validator detects bad objects in plan."""
+    a, b, c = constants("a b c")
+    a1_name = "action_1"
+    a2_name = "action_2"
+    plan = Plan(actions=[(a1_name, [a, b, c]), (a2_name, [b, c, a])])
+
+    a, b = constants("a b")
+    x, y, z = variables("x y z")
+    p = Predicate("p", x, y, z)
+    action_1 = Action("action_1", [x, y, z], precondition=p, effect=Not(p))
+    action_2 = Action("action_2", [x, y, z], precondition=Not(p), effect=p)
+    domain = Domain(
+        "simple_domain", constants={a}, predicates={p}, actions={action_1, action_2}
+    )
+
+    d, e, f = constants("d e f")
+    problem = Problem("simple_problem", domain, objects={d, e, f})
+
+    with pytest.raises(
+        PDDLValidationError,
+        match="Some parameters of .* are not in the problem objects.",
+    ):
+        plan.check(domain, problem)
 
 
 def test_plan_instantiation_eq():
